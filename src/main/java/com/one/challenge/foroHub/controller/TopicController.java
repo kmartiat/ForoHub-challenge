3948bf8a561f79931c304336e7deb6bb43e7c6ee
kmartiat <12090460@unmsm.edu.pe>
2024-07-12 19:42:15 -0500

    add GlobalExceptionHandler and implement get a topic by id

--- src/main/java/com/one/challenge/foroHub/controller/TopicController.java
+++ src/main/java/com/one/challenge/foroHub/controller/TopicController.java
@@ -4,17 +4,16 @@ import com.one.challenge.foroHub.domain.Topic;
 import com.one.challenge.foroHub.dto.request.RequestTopicDto;
 import com.one.challenge.foroHub.dto.response.ResponseTopicDto;
 import com.one.challenge.foroHub.repository.ITopicRepository;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/topics")
@@ -30,5 +29,14 @@ public class TopicController {
         Topic topic = repository.save(new Topic(request));
         URI uri = uriComponentsBuilder.path("/api/v1/topics/{id}").buildAndExpand(topic.getId()).toUri();
         return ResponseEntity.created(uri).body(new ResponseTopicDto(topic));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponseTopicDto> gerTopicById(@PathVariable Long id) {
+        Optional<Topic> topic = repository.findById(id);
+        if (topic.isEmpty()) {
+            throw new EntityNotFoundException();
+        }
+        return ResponseEntity.ok(new ResponseTopicDto(topic.get()));
     }
 }
